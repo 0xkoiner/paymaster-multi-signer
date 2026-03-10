3078995a8a0732b682de28e22b2dbd838cb34a8a
0xKoiner <alexgray.bcn.esp@gmail.com>
2026-03-10 17:44:21 +0100

    P256

---
+++ test/data/Etch.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.34;
+
+import { Data } from "./Data.t.sol";
+import { Constants } from "./Constants.sol";
+
+contract Etch is Data {
+    // Etch Deployed Bytecode
+    function _ethc() internal {
+        vm.etch(Constants.P256_ADDRESS, Constants.P256_DEPLOYED_BYTE_CODE);
+    }
+
+    // Label ERC5564 and ERC6538 canonical addresses
+    function _label() internal {
+        vm.label(Constants.P256_ADDRESS, "P256-Verifier");
+    }
+}
